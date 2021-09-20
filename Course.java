dd00fd8bd83b759dfbef00d299dcafe0d9106739
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2021-09-20 15:05:11 +0530

    My Second commit

---
+++ Course.java
@@ -0,0 +1,7 @@
+public class Course {
+
+public static void main(String args[]) {
+
+System.out.println("Hello Git from course class!");
+}
+}
--- Customer.java
+++ Customer.java
@@ -2,6 +2,7 @@ public class Customer {
 
 public static void main(String args[]) {
 
-System.out.println("Hello from Git!");
+System.out.println("Hello Git from Customer!");
+System.out.println("Hello from Git from Customer Again!");
 }
 }
