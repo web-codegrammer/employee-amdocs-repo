3a5487f891ba71915ed89916a8e6ce147a50a3c1
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2021-09-27 14:26:24 +0530

    Fifth commit

--- Customer.java
+++ Customer.java
@@ -4,5 +4,5 @@ public static void main(String args[]) {
 
 System.out.println("Hello Git from Customer!");
 System.out.println("Hello from Git from Customer Again!");
-}
+System.out.println("Autotrigger from git!");
 }
