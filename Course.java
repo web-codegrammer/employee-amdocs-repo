55fca45e9d6d139be5a2b431dab1f7146a56813a
Devanshu Vashishtha <sharmadevanshu452@gmail.com>
2021-09-27 14:29:13 +0530

    Sixth commit

--- Course.java
+++ Course.java
@@ -4,6 +4,6 @@ public static void main(String args[]) {
 
 System.out.println("Hello Jenkins from course class!");
 System.out.println("Hello Git and Jenkins from course class!");
-System.out.println("Autocommit from github!");
+System.out.println("Autotrigger from github!");
 }
 }
